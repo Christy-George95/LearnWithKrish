f131ee143f87a4f9e014f8d7535a9a3100ad7ec9
Christy <christygeorge1995@gmail.com>
2022-09-08 14:34:26 +0530

    Spring Boot Kafka Messaging App

---
+++ SpringBootKafka/SpringbootKafka/src/main/java/com/example/kafkasample/Kafka/KafkaConsumer.java
@@ -0,0 +1,18 @@
+package com.example.kafkasample.Kafka;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+@Service
+public class KafkaConsumer {
+	
+	public static final Logger LOGGER = LoggerFactory.getLogger(KafkaConsumer.class);
+	
+	@KafkaListener(topics = "demo", groupId ="myGroup")
+	public void consume(String message) {
+		LOGGER.info(String.format("Message received -> %s ", message));
+	}
+
+}
